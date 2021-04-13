c25cc8ee15c390e630d84610828735aaa50d153b
Tatum Bray <tatumrbray@gmail.com>
2021-04-13 13:52:44 -0600

    working on displaying image

--- App.js
+++ App.js
@@ -6,17 +6,35 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>This is a Text component</Text>
-      <Image></Image>
+      <ImageDisplay />
       <StatusBar style='auto' />
     </View>
   )
 }
 
+const ImageDisplay = () => {
+  return (
+    <View style={styles.imageContainer}>
+      <Image
+        style={styles.cigaretteContainer}
+        source={require("./components/images/ironic-cigarette.jpg")}
+      />
+    </View>
+  )
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  cigaretteContainer: {
+    width: 2,
+    height: 2,
+  },
+  imageContainer: {
+    flex: 2,
+    alignItems: "center",
   },
 })
