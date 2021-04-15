7970a5ed9862a3c447ef8ab7cf218ea4339be2c9
Tatum Bray <tatumrbray@gmail.com>
2021-04-14 21:02:27 -0600

    still messing around

--- App.js
+++ App.js
@@ -1,40 +1,42 @@
 import { StatusBar } from "expo-status-bar"
 import React from "react"
-import { StyleSheet, Text, View, Image } from "react-native"
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This is a Text component</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLine={1} onPress={() => console.log("texrt")}>
+        why my pic no show?
+      </Text>
       <ImageDisplay />
       <StatusBar style='auto' />
-    </View>
+    </SafeAreaView>
   )
 }
 
 const ImageDisplay = () => {
   return (
-    <View style={styles.imageContainer}>
+    <SafeAreaView style={styles.imageContainer}>
       <Image
         style={styles.cigaretteContainer}
         source={require("./components/images/ironic-cigarette.jpg")}
       />
-    </View>
+    </SafeAreaView>
   )
 }
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
+    flex: 0.5,
+    backgroundColor: "dodgerblue",
     alignItems: "center",
     justifyContent: "center",
   },
   cigaretteContainer: {
-    width: 2,
-    height: 2,
+    width: 1,
+    height: 1,
   },
   imageContainer: {
-    flex: 2,
+    flex: 0.5,
     alignItems: "center",
   },
 })
